d1593ef30a729a79f1f37b6ab1fdb716767e938e
Louis-Francis Ratté-Boulianne <louis-francis.ratte-boulianne@collabora.co.uk>
2009-08-04 15:50:08 -0400

    Fixed the copyright holder

--- butterfly/capabilities.py
+++ butterfly/capabilities.py
@@ -1,6 +1,6 @@
 # telepathy-butterfly - an MSN connection manager for Telepathy
 #
-# Copyright (C) 2009 Youness Alaoui <youness.alaoui@collabora.co.uk>
+# Copyright (C) 2009 Collabora Ltd.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

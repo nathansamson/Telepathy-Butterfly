eb4ec881377241f49d757af88e5af1f3e1ee4bca
Jonny Lamb <jonny.lamb@collabora.co.uk>
2010-04-04 00:51:25 +0100

    channel: add ButterflyChannel class to deal with Initiator props
    Signed-off-by: Jonny Lamb <jonny.lamb@collabora.co.uk>

--- butterfly/channel/__init__.py
+++ butterfly/channel/__init__.py
@@ -15,3 +15,47 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+import logging
+import dbus
+
+import telepathy
+
+from butterfly.handle import ButterflyHandleFactory
+
+__all__ = ['ButterflyChannel']
+
+logger = logging.getLogger('Butterfly.Channel')
+
+class ButterflyChannel(object):
+    def __init__(self, conn, props):
+        # If we have InitiatorHandle set in our new channel, use that,
+        if telepathy.CHANNEL_INTERFACE + '.InitiatorHandle' in props:
+            self._initiator = conn.handle(telepathy.HANDLE_TYPE_CONTACT,
+                props[telepathy.CHANNEL_INTERFACE + '.InitiatorHandle'])
+
+        # otherwise use InitiatorID.
+        elif telepathy.CHANNEL_INTERFACE + '.InitiatorID' in props:
+            self._initiator = ButterflyHandleFactory(conn, 'contact',
+                id=props[telepathy.CHANNEL_INTERFACE + '.InitiatorHandle'])
+
+        # If we don't have either of the above but we requested the channel,
+        # then we're the initiator.
+        elif props[telepathy.CHANNEL_INTERFACE + '.Requested']:
+            self._initiator = conn.GetSelfHandle()
+
+        else:
+            logger.warning('InitiatorID or InitiatorHandle not set on new channel')
+            self._initiator = None
+
+        # Don't implement the initiator properties if we don't have one.
+        if self._initiator:
+            self._implement_property_get(telepathy.CHANNEL_INTERFACE, {
+                    'InitiatorHandle': lambda: dbus.UInt32(self._initiator.id),
+                    'InitiatorID': lambda: self._initiator.name
+                    })
+
+            self._add_immutables({
+                    'InitiatorHandle': telepathy.CHANNEL_INTERFACE,
+                    'InitiatorID': telepathy.CHANNEL_INTERFACE,
+                    })
